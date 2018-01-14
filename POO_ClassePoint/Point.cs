e21fd45065edda28209586da6c0ebb8231865634
ludo <ludo@LUDO>
2018-01-14 18:15:26 +0100

    Ajoutez des fichiers projet.

---
+++ POO_ClassePoint/Point.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ClassePoint
+{
+    class Point
+    {
+        private double Abscisse;            //L'attribut Abscisse
+        private double Ordonne;             //L'attribut Ordonne
+
+        public double getAbscisse()         //Le getter de l'attribut Abscisse
+        {
+            return Abscisse;
+        }
+
+        public void setAbscisse(double a)   //Le setter de l'attribut Abscisse
+        {
+            Abscisse = a;
+        }
+
+        public double getOrdonne()          //Le getter de l'attribut Ordonne
+        {
+            return Ordonne;
+        }
+
+        public void setOrdonne(double o)    //Le setter de l'attribut Ordonne
+        {
+            Ordonne = o;
+        }
+
+        public Point()                     //Le constructeur par défaut
+        {
+        }
+
+        public Point(double a, double o)   //Le constructeur d'initialisation
+        {
+            Abscisse = a;
+            Ordonne = o;
+        }
+
+        public double Norme()              //La méthode Norme
+        {
+            return Math.Sqrt(Abscisse * Abscisse + Ordonne * Ordonne);
+        }
+    
+    }
+}
---
+++ POO_ClassePoint/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ClassePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, o, n;
+            Point p = new Point();//L'instanciation d'un objet Point
+            Console.Out.Write("Donner l'abscisse: ");
+            a = double.Parse(Console.In.ReadLine());
+            Console.Out.Write("Donner l'ordonne: ");
+            o = double.Parse(Console.In.ReadLine());
+            //l'appel des méthodes sur l'objet p
+            p.setAbscisse(a);
+            p.setOrdonne(o);
+            n = p.Norme();
+            Console.Out.WriteLine("La norme du point (" + p.getAbscisse() + "," + p.getOrdonne() + ") est: " + n);
+            Console.ReadKey();
+            
+        }
+    }
+}
